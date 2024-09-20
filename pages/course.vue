014634fb6814a59398b4bde986a6aa372bc10430
Ilya Gorbul <ilyadrbn@gmail.com>
2024-09-20 19:43:10 +0300

    creating navbar

--- pages/course.vue
+++ pages/course.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const { chapters } = useCourse();
+
+console.log(chapters);
+</script>
+
 <template>
     <div
         class="flex h-full min-h-screen w-full flex-col items-center bg-gray-100 p-12"
@@ -15,7 +21,21 @@
             <div
                 class="prose mr-4 flex min-w-[20ch] max-w-[30ch] flex-col rounded-md bg-white p-8"
             >
-                <h3 class="font-bold">Chapters</h3>
+                <h3 class="mb-6 font-bold">Chapters</h3>
+                <nav>
+                    <ol class="flex flex-col gap-4 space-y-1">
+                        <li v-for="chapter in chapters" :key="chapter.slug">
+                            <h4 class="font-bold">{{ chapter.title }}</h4>
+                            <NuxtLink
+                                v-for="(lesson, index) in chapter.lessons"
+                                :key="lesson.downloadUrl"
+                                class="mb-2 inline-block font-medium text-slate-400 hover:text-slate-700"
+                                :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+                                >{{ index + 1 }}. {{ lesson.title }}</NuxtLink
+                            >
+                        </li>
+                    </ol>
+                </nav>
             </div>
 
             <div class="prose w-[65ch] rounded-md bg-white p-12">
@@ -24,7 +44,3 @@
         </div>
     </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- pages/course/chapter/[chapterId]/lesson/[lessonId].vue
+++ pages/course/chapter/[chapterId]/lesson/[lessonId].vue
@@ -2,24 +2,63 @@
 const course = useCourse();
 const route = useRoute();
 
-const chapter: ComputedRef = computed(() => {
-    return course.chapters.find(
-        (chapter) => chapter.slug === route.params.chapterId,
+interface ILesson {
+    downloadUrl?: string;
+    sourceUrl?: string;
+    slug: string;
+    number: number;
+    text: string;
+    title: string;
+    videoId?: number;
+}
+
+interface IChapter {
+    title: string;
+    slug: string;
+    number: number;
+    lessons: ILesson[];
+}
+
+const chapter: ComputedRef = computed((): IChapter | null => {
+    return (
+        course.chapters.find(
+            (chapter) => chapter.slug === route.params.chapterId,
+        ) ?? null
     );
 });
 
-console.log(chapter.value);
-
-const lesson: ComputedRef = computed(() => {
-    return chapter.value.lessons.find(
-        (lesson: { slug: string }) => lesson.slug === route.params.lessonId,
+const lesson: ComputedRef = computed((): ILesson | null => {
+    return (
+        chapter.value.lessons.find(
+            (lesson: { slug: string }) => lesson.slug === route.params.lessonId,
+        ) ?? null
     );
 });
 </script>
 
 <template>
-    <h2>Lesson</h2>
-    <p>This is a lesson</p>
-    <p>{{ chapter.title }}</p>
-    <p>{{ lesson.title }}</p>
+    <div>
+        <p class="mb-1 mt-0 font-bold uppercase text-slate-400">
+            Lesson {{ chapter.number }} - {{ lesson.number }}
+        </p>
+        <h2 class="m-0 text-3xl font-bold">{{ lesson.title }}</h2>
+        <div class="mb-8 mt-2 flex space-x-4">
+            <a
+                v-if="lesson.sourceUrl"
+                class="text-md text-grey-500 font-normal"
+                :href="lesson.sourceUrl"
+            >
+                Download Source Code
+            </a>
+            <a
+                v-if="lesson.downloadUrl"
+                class="text-md text-grey-500 font-normal underline hover:text-blue-500"
+                :href="lesson.downloadUrl"
+            >
+                Download Video
+            </a>
+        </div>
+        <VideoPlayer v-if="lesson.videoId" :video-id="lesson.videoId" />
+        <p>{{ lesson.text }}</p>
+    </div>
 </template>
