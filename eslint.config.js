9f9a089e380b2febef44e258b42133a8b241841f
Ilya Gorbul <ilyadrbn@gmail.com>
2024-09-20 14:45:13 +0300

    install dependecies, create template for course page

--- eslint.config.js
+++ eslint.config.js
@@ -15,4 +15,9 @@ export default [
     files: ["**/*.vue"],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
   },
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
 ];
---
+++ pages/course.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="flex h-full min-h-screen w-full flex-col items-center bg-gray-100 p-12"
+    >
+        <div class="prose mb-12">
+            <h1>
+                <span class="font-medium">
+                    Course:
+                    <span class="font-bold">Mastering Nuxt 3</span>
+                </span>
+            </h1>
+        </div>
+
+        <div class="flex flex-grow flex-row justify-center">
+            <div
+                class="prose mr-4 flex min-w-[20ch] max-w-[30ch] flex-col rounded-md bg-white p-8"
+            >
+                <h3 class="font-bold">Chapters</h3>
+            </div>
+
+            <div class="prose w-[65ch] rounded-md bg-white p-12">
+                <h2 class="font-bold">Lesson</h2>
+                <p>This is a lesson</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
